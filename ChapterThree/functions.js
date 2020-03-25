a9f148f4e64f34ffe3dc416ac879470e943763b9
citizenseven <adrianx67@gmail.com>
2020-03-25 19:01:05 +0300

    lessen_3

---
+++ ChapterThree/functions.js
@@ -0,0 +1,48 @@
+
+
+//Замыкание
+// let c = 4;
+// function addX(x){
+//     return function(n){
+//         return n + x;
+//     }
+// }
+// const addThree = addX(3);
+// let d = addThree(c);
+// console.log(d);
+
+// function creatCounter(){
+//     let counter = 0;
+//     const myFunc = function(){
+//         counter = counter + 1;
+//         return counter;
+//     }
+//     return myFunc;
+// }
+// const increment = creatCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// const c4 = increment();
+// console.log(c1,c2,c3,c4);
+
+// let val = 7;
+// function createAdder(){
+//     function addNumber(a,b){
+//         let ret = a + b;
+//         return ret;
+//     }
+//     return addNumber;
+// }
+// let adder = createAdder();
+// let sum = adder(val, 8);
+// console.log(sum);
+
+// let val1 = 2;
+// function multiplyThis(n){
+//     let ret = n * val1;
+//     return ret;
+// }
+// let multiplied = multiplyThis(6);
+// console.log(multiplied);
+
